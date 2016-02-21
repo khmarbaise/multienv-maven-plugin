c76c54464de9dd4fade74c5869a4eb77f5ea95e3
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-02-21 16:11:36 +0100

    First test to get MavenArchiver injected.

--- src/main/java/com/soebes/maven/plugins/configuration/ConfigurationMojo.java
+++ src/main/java/com/soebes/maven/plugins/configuration/ConfigurationMojo.java
@@ -1,9 +1,12 @@
 package com.soebes.maven.plugins.configuration;
 
+import org.apache.maven.archiver.MavenArchiver;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.util.DirectoryScanner;
 
 /**
@@ -17,19 +20,23 @@ public class ConfigurationMojo
     extends AbstractConfigurationMojo
 {
 
+    @Component( role = Archiver.class, hint = "jar" )
+    private MavenArchiver archiver;
+
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
-
         DirectoryScanner ds = new DirectoryScanner();
         ds.setBasedir( getSourceDirectory() );
+        ds.setExcludes( new String[] { "." } );
+        ds.addDefaultExcludes();
 
         ds.scan();
 
         String[] includedDirectories = ds.getIncludedDirectories();
         for ( String folder : includedDirectories )
         {
-            getLog().info( "Environment Folder. " + folder );
+            getLog().info( "Environment Folder: '" + folder + "'" );
         }
     }
 
